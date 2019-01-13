2f7a95ac87eae55e6ed9b0ed04ffd1798d64ba75
Milad Hazrati <miladhazrati75@gmail.com>
2019-01-14 00:55:11 +0330

    add delete post

--- routes.py
+++ routes.py
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cms.db'
 
 db = SQLAlchemy(app)
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -20,13 +21,13 @@ class User(db.Model):
     password = db.Column(db.String(40), unique=True)
     name = db.Column(db.String(40), unique=True)
 
+
 class Post(db.Model):
     __tablename__ = 'posts'
     pid = db.Column(db.Integer, primary_key=True)
     uid = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(40), unique=True)
     body = db.Column(db.String, unique=True)
-    #name = db.Column(db.String(40), unique=True)
 
 
 @app.route('/')
@@ -61,6 +62,7 @@ def login():
 def logout():
     del session['username']
     del session['logged']
+    session['msg'] = 'logged out!'
     return render_template('index.html')
 
 
@@ -112,22 +114,21 @@ def edit_profile():
 
 @app.route('/Posts')
 def show_posts():
-    qpost = Post.query.filter(Post.uid == session['uid']).limit(5).all()
+    qpost = Post.query.filter(Post.uid == session['uid']).all()
     title = []
     for i in range(0,5):
         try:
             title.append(qpost[i].title)
         except IndexError:
             continue
-
-        #author = session['name']
-        #body = qpost.body
     return render_template('posts.html', post=title)
+
 
 @app.route('/NewPost')
 def show_new_post():
     session['form'] = 'post'
     return render_template('posts.html', post=session)
+
 
 @app.route('/ValidatePost', methods=['GET','POST'])
 def new_post():
@@ -135,7 +136,33 @@ def new_post():
     db.session.add(post)
     db.session.commit()
     session['form'] = ''
+    session['msg'] = 'Post Created!'
     return redirect('/Posts')
+
+
+@app.route('/Post/<title>')
+def show_post(title):
+    qpost = Post.query.filter(Post.uid == session['uid'], Post.title == title).first()
+    ptitle = qpost.title
+    pbody = qpost.body
+    session['post'] = True
+    return render_template('showpost.html', content=(ptitle, pbody))
+
+
+@app.route('/Post/<title>/Delete')
+def delete_post(title):
+    qpost = Post.query.filter(Post.uid == session['uid'], Post.title == title).first()
+    db.session.delete(qpost)
+    db.session.commit()
+    qpost = Post.query.filter(Post.uid == session['uid']).all()
+    title = []
+    for i in range(len(qpost)):
+        try:
+            title.append(qpost[i].title)
+        except IndexError:
+            continue
+    session['msg'] = 'Post Deleted!'
+    return render_template('posts.html', post=title)
 
 
 if __name__ == '__main__':
